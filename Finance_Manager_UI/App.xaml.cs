f1cbe67230c283e5cc8252e18c80a22a5d1e1496
kgeyer <karel.geyer@nakit.cz>
2024-03-19 23:17:32 +0100

    Add UI project using MAUI for multi-platform development.

---
+++ Finance_Manager_UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Finance_Manager_UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Finance_Manager_UI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Finance_Manager_UI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
