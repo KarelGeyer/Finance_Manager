63a39ea95d8b51be1e6826df7ca75f19f5078402
kgeyer <karel.geyer@nakit.cz>
2024-03-20 12:18:50 +0100

    Add showcase test case

--- UserService.Test/Controllers/UserControllerTest.cs
+++ UserService.Test/Controllers/UserControllerTest.cs
@@ -1,9 +1,23 @@
+using Common.User;
+using UsersService.Controllers;
+
 namespace UserService.Test.Controllers
 {
     [TestClass]
     public class UserControllerTest
     {
         [TestMethod]
-        public void GetUsersTestItRetrievesUsers() { }
+        public async Task GetUsersTestItRetrievesUsers()
+        {
+            //Arrange
+            UserController controller = new UserController();
+
+            //Act
+            List<User> result = await controller.GetUsers();
+            //Assert
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Count);
+        }
     }
 }
