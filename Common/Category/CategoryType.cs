165b8538345b952c7302bb4108fe0ef04f1f3216
kgeyer <karel.geyer@nakit.cz>
2024-03-25 01:08:11 +0100

    Add Base Category DB models and an Enumerator

---
+++ Common/Category/CategoryType.cs
@@ -0,0 +1,21 @@
+﻿using Common.Enums;
+using Postgrest.Attributes;
+using Postgrest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Category
+{
+    [Table("CategoryTypes")]
+    public class CategoryType : BaseModel
+    {
+        [PrimaryKey("Id", false)]
+        public int Id { get; set; }
+
+        [Column("Value")]
+        public ECategoryType Value { get; set; }
+    }
+}
