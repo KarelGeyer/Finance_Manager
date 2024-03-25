cc56c5c1e735a3119edac9b1c4d3fc4e906b72da
kgeyer <karel.geyer@nakit.cz>
2024-03-25 01:09:51 +0100

    Add new Category Service and few base Get Methods

---
+++ Services/CategoryService/Program.cs
@@ -0,0 +1,49 @@
+using System;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Supabase;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Supabase configuration
+var configuration = builder.Configuration;
+
+var supabaseUrl = configuration["SupaBase:Url"];
+var supabaseKey = configuration["SupaBase:Key"];
+
+builder.Services.AddSingleton(_ =>
+{
+    var options = new Supabase.SupabaseOptions { AutoConnectRealtime = true };
+
+    var client = new Supabase.Client(supabaseUrl!, supabaseKey, options);
+    client.InitializeAsync().Wait();
+
+    return client;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
