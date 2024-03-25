3aae1ea6db6f06dfad6c4a4ea166183752ed00d9
kgeyer <karel.geyer@nakit.cz>
2024-03-25 16:49:56 +0100

    KAN-69 Add a new DB service and Interface to a Category Service Project.

---
+++ Services/CategoryService/Db/IDbService.cs
@@ -0,0 +1,28 @@
+﻿using Common;
+using Common.Category;
+using Common.Enums;
+
+namespace CategoryService.Service
+{
+    public interface IDbService<T>
+        where T : BaseDbModel, new()
+    {
+        /// <summary>
+        /// Retrieves all categories from the db
+        /// </summary>
+        /// <returns>Task with list of categories</returns>
+        Task<List<T>> GetAllAsync();
+
+        /// <summary>
+        /// Attempts to find a category using provided id
+        /// </summary>
+        /// <returns>Task with a category</returns>
+        Task<T> GetAsync(int id);
+
+        /// <summary>
+        /// Retrieves all categories of a given type from the db
+        /// </summary>
+        /// <returns>Task with list of categories</returns>
+        Task<List<Category>> GetByCategoryAsync(int categoryTypeId);
+    }
+}
