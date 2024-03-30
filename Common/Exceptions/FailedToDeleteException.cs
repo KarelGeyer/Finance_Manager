c666f34aaa89a88d88e8e56bbfbf522c0b65b7ee
kgeyer <karel.geyer@nakit.cz>
2024-03-30 15:52:48 +0100

    KAN-68
    The most significant changes involve the addition of new classes and constructors, as well as the enhancement of existing classes with summary comments and new methods. The `FailedToCreateException<T>` class was added to handle exceptions when an instance creation fails. The `FailedToDeleteException<T>`, `FailedToUpdateException<T>`, `NotFoundException`, and `UserDoesNotExistException` classes were updated with summary comments and new constructors. The `CustomResponseMessage` class was enhanced with new methods to handle failed operations, and the `BaseDbModel` class was updated with a summary comment for the `Id` property.
    
    1. The `FailedToUpdateException<T>` class was removed from the `FailedToCreateException - Copy.cs` file.
    2. A new class `FailedToCreateException<T>` was added to the `FailedToCreateException.cs` file. This class handles exceptions when an instance creation of type `T` fails. It includes two constructors, one without parameters and one with an `id` parameter.
    3. The `FailedToDeleteException<T>` class in the `FailedToDeleteException.cs` file was updated with summary comments. A new constructor was added that takes an `id` parameter.
    4. The `FailedToUpdateException<T>` class in the `FailedToUpdateException.cs` file was updated. The class now handles exceptions when a failed update operation occurs. The constructors were updated to reflect this change.
    5. The `NotFoundException` class in the `NotFoundException.cs` file was updated with summary comments. A new constructor was added that takes an `id` parameter.
    6. The `UserDoesNotExistException` class in the `UserDoesNotExistException.cs` file was updated with summary comments.
    7. The `CustomResponseMessage` class in the `ResponseMessage.cs` file was updated with summary comments. New methods were added to get string information of a failed creation, update, or deletion of an object.
    8. The `BaseDbModel` class in the `BaseDbModel.cs` file was updated with a summary comment for the `Id` property.

--- Common/Exceptions/FailedToDeleteException.cs
+++ Common/Exceptions/FailedToDeleteException.cs
@@ -3,12 +3,23 @@ using System;
 
 namespace Common.Exceptions
 {
+    /// <summary>
+    /// Represents an exception that is thrown when failed to delete an instance of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the object that failed to be deleted.</typeparam>
     public class FailedToDeleteException<T> : Exception
         where T : class
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FailedToDeleteException{T}"/> class.
+        /// </summary>
         public FailedToDeleteException()
             : base(CustomResponseMessage.GetFailedToDeleteMessage<T>()) { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FailedToDeleteException{T}"/> class with the specified ID.
+        /// </summary>
+        /// <param name="id">The ID associated with the failed deletion.</param>
         public FailedToDeleteException(int id)
             : base(CustomResponseMessage.GetFailedToDeleteMessage<T>(id)) { }
     }
--- Common/Exceptions/NotFoundException.cs
+++ Common/Exceptions/NotFoundException.cs
@@ -8,14 +8,21 @@ using System.Xml.Linq;
 namespace Common.Exceptions
 {
     /// <summary>
-    /// A Custom Exception throwen when a record or a list of records are not found in the database
+    /// A Custom Exception thrown when a record or a list of records are not found in the database
     /// </summary>
     [Serializable]
     public class NotFoundException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotFoundException"/> class.
+        /// </summary>
         public NotFoundException()
             : base(string.Format("No record was found")) { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotFoundException"/> class with the specified id.
+        /// </summary>
+        /// <param name="id">The id of the record.</param>
         public NotFoundException(int id)
             : base(string.Format("No record by id {0} was found", id)) { }
     }
