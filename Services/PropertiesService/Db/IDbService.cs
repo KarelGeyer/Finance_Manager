570b1edf60544e68418279a9784e8c6313290d48
GannyK <77975797+KarelGeyer@users.noreply.github.com>
2024-04-14 18:57:44 +0200

    adjust db service

--- Services/PropertiesService/Db/IDbService.cs
+++ Services/PropertiesService/Db/IDbService.cs
@@ -6,5 +6,18 @@ namespace LoansService.Db
     {
         Task<List<Property>> GetAll(int ownerId);
 
+        Task<Property> Get(int ownerId);
+
+        Task<List<Property>> GetByCategory(int ownerId, int categoryId);
+
+        Task<bool> Create(CreateProperty createProperty);
+
+        Task<bool> UpdateName(int ownerId, string name);
+
+        Task<bool> UpdateValue(int ownerId, double value);
+
+        Task<bool> DeleteById(int ownerId, int id);
+
+        Task<bool> DeleteAll(int ownerId);
     }
 }
