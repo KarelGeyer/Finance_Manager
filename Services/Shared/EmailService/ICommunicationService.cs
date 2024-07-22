ab641df8cd11ad2bd5f4bcec54ff327151eb786e
kgeyer <karel.geyer@nakit.cz>
2024-07-22 13:12:03 +0200

    Create shared common library services to be reused across all services whenever and wherever needed.

---
+++ Services/Shared/EmailService/ICommunicationService.cs
@@ -0,0 +1,10 @@
+﻿using Common.Models.Email;
+using System.Net.Mail;
+
+namespace EmailService
+{
+    public interface ICommunicationService
+    {
+        public EmailResponse SendEmail(Email email);
+    }
+}
